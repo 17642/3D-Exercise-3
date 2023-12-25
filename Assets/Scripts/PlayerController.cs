6f5073e2efb20cc373330954b7e4bf426d37b7a7
상빈 <67950060+17642@users.noreply.github.com>
2023-12-25 09:07:32 +0900

    3-4

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,11 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     private Rigidbody playerRb;
     private Animator amim;
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    public AudioSource playerAudio;
 
     public float jumpForce;
     public float gravityModifier;
@@ -17,6 +22,7 @@ public class PlayerController : MonoBehaviour
     {
         playerRb = GetComponent<Rigidbody>();
         amim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics.gravity *= gravityModifier;
         //playerRb.AddForce(Vector3.up * 1000);//게임이 시작할 때 플레이어가 하늘로 날아감
     }
@@ -29,6 +35,9 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);//스페이스바를 누르면 하늘로 날아감.
             isOnGround = false;//땅이랑은 닿고 있지 않다고 판단,
             amim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
+
         }
     }
 
@@ -37,6 +46,7 @@ public class PlayerController : MonoBehaviour
         if (collision.transform.CompareTag("Ground"))
         {
             isOnGround = true; // Ground와 닿으면 isOnGround = true
+            dirtParticle.Play();
         }
         if (collision.transform.CompareTag("Obstacle"))
         {
@@ -44,6 +54,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("GAME OVER!");
             amim.SetBool("Death_b", true);
             amim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
